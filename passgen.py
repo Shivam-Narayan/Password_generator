71746ec06adbc1579f12911cbd7f2c9e5b4abf5d
Shivam Narayan <77800690+Shivam-Narayan@users.noreply.github.com>
2023-08-11 12:06:34 +0530

    Code

---
+++ passgen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + " !@#$%^&*()")
+
+def generate_password():
+    password_length = int(input("How long would you like your password to be? "))
+    
+    random.shuffle(characters)
+    
+    password = []
+    
+    for x in range(password_length):
+        password.append(random.choice(characters))
+    
+    random.shuffle(password)
+    
+    password = "" .join(password)
+    print(password)
+
+option = input("Do you want to generate a password? (Yes/No): ")
+
+if option == "yes":
+    generate_password()
+elif option == "no":
+    print("Program ended")
+    quit()
+else:
+    print("Invalid input, please input Yes or No")
+    quit()
+
+
+# In[ ]:
+
+
+
+
